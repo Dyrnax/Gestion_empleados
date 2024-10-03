db7d41c4638f68c99fddde4d4fe68dd97910f00d
Nicolas C <nicolasc101004@gmail.com>
2024-10-02 22:53:49 -0300

    Faltan varificar edad y fecha de contrato

---
+++ Empleados.py
@@ -0,0 +1,31 @@
+
+import re 
+
+
+class Empleados:
+    def __init__(self, id_empleado, nombre_empleado, id_cargo, direccion, numero_de_telefono, correo, fecha_de_inicio_de_contrato, salario, rut, fecha_nacimiento):
+        self.id_empleado = id_empleado
+        self.nombre_empleado = nombre_empleado
+        self.id_cargo = id_cargo
+        self.direccion = direccion
+        self.numero_de_telefono = numero_de_telefono
+        self.correo = correo
+        self.fecha_de_inicio_de_contrato = fecha_de_inicio_de_contrato
+        self.salario = salario
+        self.rut = rut
+        self.fecha_nacimiento = fecha_nacimiento
+        
+    def validar_correo(self):
+        pat_correo= r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if (re.match(pat_correo,self.correo)):
+            return True 
+        else:
+            return False
+        
+    def validar_telefono(self):
+        pat_telefono= r'^\+?[\d\s\-]{7,15}$'
+        if re.match(pat_telefono,self.numero_de_telefono):
+            return  True 
+        else:
+            return False
+        
