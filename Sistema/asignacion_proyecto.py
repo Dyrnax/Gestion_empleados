23d92c41d2f77c729bfb6a157b0588c3c31e871a
Nicolas C <nicolasc101004@gmail.com>
2024-10-12 11:43:09 -0300

    sacando comas

--- Sistema/asignacion_proyecto.py
+++ Sistema/asignacion_proyecto.py
@@ -3,6 +3,6 @@ import Proyecto
 
 class asignacion_proyecto(Empleados,Proyecto):
     def __init__(self,id_emplado,id_asocacion,id_proyecto):
-        Proyecto.__init__(id_proyecto),
-        Empleados.__init__(id_emplado),
+        Proyecto.__init__(id_proyecto)
+        Empleados.__init__(id_emplado)
         self.id_asocacion=id_asocacion
--- Sistema/generar_informes.py
+++ Sistema/generar_informes.py
@@ -5,9 +5,9 @@ import Registro_de_tiempo
 
 class informes(Proyecto,Empleados,departamentos,Registro_de_tiempo):
     def __init__(self,id_proyecto,id_empleado,id_departamento,id_registro):
-        Proyecto.__init__(id_proyecto),
-        Empleados.__init__(id_empleado),
-        departamentos.__init__(id_departamento),
+        Proyecto.__init__(id_proyecto)
+        Empleados.__init__(id_empleado)
+        departamentos.__init__(id_departamento)
         Registro_de_tiempo.__init__(id_registro)
         
         
