d8165aec4ba296ad48dfdf29393fb84c4942caa8
Nicolas C <nicolasc101004@gmail.com>
2024-10-14 00:41:16 -0300

    editando dado las instrucciones del profesor

--- Sistema/Registro_de_tiempo.py
+++ Sistema/Registro_de_tiempo.py
@@ -1,13 +1,11 @@
-import Empleados
-import Proyecto
+
 import Asignacion_proyecto
 
-class registro_de_tiempo(Empleados,Proyecto,Asignacion_proyecto):
-    def __init__(self,id_empleado,id_proyecto,id_asociacion,id_registro,fecha,horas_trabajadas,tareas):
-        Empleados.__init__(id_empleado)
-        Proyecto.__init__(id_proyecto)
+class registro_de_tiempo(Asignacion_proyecto):
+    def __init__(self,id_asociacion,id_registro,fecha,horas_trabajadas,tareas,observacion):
         Asignacion_proyecto.__init__(id_asociacion)
         self.id_registro=id_registro
         self.fecha= fecha
         self.horas_trabajadas=horas_trabajadas
         self.tareas=tareas
+        self.observacion=observacion
