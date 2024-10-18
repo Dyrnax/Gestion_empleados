16a126f568b097d3d7d96a53b73b6759f9285c05
Nicolas C <nicolasc101004@gmail.com>
2024-10-18 15:19:35 -0300

    Areglando metodos

--- Sistema/Empleados.py
+++ Sistema/Empleados.py
@@ -1,14 +1,15 @@
 
 import re 
-import Cargo_empleados
-import Roles
+from Cargo_empleados import cargo_empleados
+from Roles import Roles
+import bcrypt
 
-class Empleados(Cargo_empleados,Roles):
+class Empleados(cargo_empleados,Roles):
     def __init__(self,id_empleado,nombre_empleado,id_rol,id_cargo,direccion,numero_de_telefono,correo,fecha_de_inicio_de_contrato,salario,rut,fecha_nacimiento,contrasena):
         self.id_emplado=id_empleado
         self.nombre_empleado=nombre_empleado
         Roles.__init__(id_rol)
-        Cargo_empleados.__init__(id_cargo)
+        cargo_empleados.__init__(id_cargo)
         self.direccion=direccion
         self.numero_de_empleado=numero_de_telefono
         self.correo=correo
@@ -28,8 +29,16 @@ class Empleados(Cargo_empleados,Roles):
         
     def validar_telefono(self):
         pat_telefono= r'^\+?[\d\s\-]{7,15}$'
-        if re.match(pat_telefono,self.numero_de_telefono):
+        if re.match(pat_telefono,self.numero_de_empleado):
             return  True 
         else:
             return False
         
+    def encriptar_contrasena(self, contrasena):
+        salt = bcrypt.gensalt() 
+        hashed_contrasena = bcrypt.hashpw(contrasena.encode('utf-8'), salt)
+        return hashed_contrasena
+
+    
+    def desencriptar_contrasena(self, contrasena):
+        return bcrypt.checkpw(contrasena.encode('utf-8'), self.contrasena)
