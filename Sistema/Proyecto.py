cf175404f9aa9fc34bf209cfcb79607a33664212
Dyrnax <marco.calfileo@gmail.com>
2024-10-19 12:58:22 -0300

    progreso semana

--- Sistema/Proyecto.py
+++ Sistema/Proyecto.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
 
-<<<<<<< HEAD
-
 class Proyecto:
     def __init__(self,id_proyecto,nombre_proyecto,descripcion_proyecto,fecha_inicio,fecha_fin):
         self.id_proyecto = id_proyecto,
@@ -9,15 +7,3 @@ class Proyecto:
         self.descripcion_proyecto = descripcion_proyecto,
         self.fecha_inicio = fecha_inicio
         self.fecha_fin = fecha_fin
-=======
-class Proyecto:
-    def __init__(self,id_proyecto,nombre_proyecto,descripcion_proyecto,fecha_inicio,fecha_fin):
-        self.id_proyecto = id_proyecto
-        self.nombre_proyecto= nombre_proyecto
-        self.descripcion_proyecto=descripcion_proyecto
-        self.fecha_inicio=fecha_inicio
-        self.fecha_fin=fecha_fin
->>>>>>> cb2e88ff92897b294f648793751ddd6644ccfa93
-        
-    
-
